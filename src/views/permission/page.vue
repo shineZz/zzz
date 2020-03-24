e2699dc04f12e6baf007db9eec30c45627cd5a97
吴 <379748531@qq.com>
2020-03-24 08:25:10 +0800

    init

---
+++ src/views/permission/page.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app-container">
+    <switch-roles @change="handleRolesChange" />
+    222222222
+  </div>
+</template>
+
+<script>
+import SwitchRoles from './components/SwitchRoles'
+
+export default {
+  name: 'PagePermission',
+  components: { SwitchRoles },
+  methods: {
+    handleRolesChange() {
+      this.$router.push({ path: '/permission/index?' + +new Date() })
+    }
+  }
+}
+</script>
